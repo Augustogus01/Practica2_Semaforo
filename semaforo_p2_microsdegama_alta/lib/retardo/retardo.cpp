02159b29f892da9a3231a78e464cf6e5bac0f26e
Augustogus01 <augusto.rodas@ieee.org>
2023-06-19 23:38:00 -0500

    Creo implementacion para retardos
    Aqui me di cuenta que no esta compilando con el platform IO y tampoco me deja escoger la tarjeta para realizar las pruebas

--- semaforo_p2_microsdegama_alta/lib/retardo/retardo.cpp
+++ semaforo_p2_microsdegama_alta/lib/retardo/retardo.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
-int16_t nbDelay(int16_t t_delay) {
+
+int16_t nbDelay(int16_t t_delay)
+{
   int16_t t_base=1;
   static int16_t count=0;
   int16_t n_time=t_delay/t_base;                      // # muestras por delay
